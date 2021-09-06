64c24cbd7536196b5e22fb6255f8de0af0615f0a
안광민 <@192.168.219.113>
2021-09-06 15:44:36 +0900

    09/06 15:44

--- TravelShare/src/main/webapp/resources/js/header.js
+++ TravelShare/src/main/webapp/resources/js/header.js
@@ -3,20 +3,21 @@ const navlinks = document.querySelectorAll(".nav-links li");
 const nav = document.querySelector(".nav-links"); 
 const guide_header = document.querySelector("#guide_header");
 const guide_link = document.querySelectorAll(".guide_link");
+
 window.addEventListener('scroll', function(e){
 	if (window.scrollY > 0) {
 		guide_header.classList.add('down');
 		for (let i = 0; i < guide_link.length; i++) {
 			guide_link[i].style.color= 'black';	
 		}
-
 		
 	} else {
 		if (guide_header.classList.contains('down'))
 		guide_header.classList.remove('down');
 		for (let i = 0; i < guide_link.length; i++) {
-			guide_link[i].style.color= 'blanchedalmond';	
+			guide_link[i].style.color= 'black';			
 		}
+		
 	}
 });
 
@@ -25,13 +26,14 @@ const navAnimation = () => {
   navlinks.forEach((link, index) => {
     // 애니메이션이 있을 때 
     if (link.style.animation) { 
-      // 애니메이션 비움 
+	// 애니메이션 비움 
       link.style.animation = ""; 
     } else { 
       // 애니메이션 없을 때 애니메이션을 추가 
       // 딜레이 간격을 줘서 li가 하나씩 차례대로 나타나도록 설정 
       link.style.animation = `navLinkFade 0.5s ease forwards ${
-        index / 7 + 0.5 }s`; 
+        index / 7 + 0.5 }s`;		
+	
       } 
     }); 
   };
